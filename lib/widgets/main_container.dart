d597e10b11fb220835f1c5f6c9c46bfb64331c9f
Mahmud Chamtooni <mudisheikh@outlook.com>
2021-06-15 16:21:48 +0000

    remove SingleChildScrollView to provent TabBarView error

--- lib/widgets/main_container.dart
+++ lib/widgets/main_container.dart
@@ -5,31 +5,35 @@ class MainContainer extends StatelessWidget {
   final Widget child;
   @override
   Widget build(BuildContext context) {
-    return SingleChildScrollView(
-      child: LayoutBuilder(builder: (context, constraint) {
-        if (constraint.maxWidth < 1000) {
-          return SingleChildScrollView(
-            child: Container(
-              color: Colors.white,
-              width: MediaQuery.of(context).size.width,
-              // height: MediaQuery.of(context).size.height,
-              child: child,
-            ),
-          );
-        } else {
-          return SingleChildScrollView(
-            child: Container(
-              margin: EdgeInsets.symmetric(
-                  horizontal: MediaQuery.of(context).size.width * 0.05,
-                  vertical: 10),
-              color: Colors.white,
-              width: MediaQuery.of(context).size.width * 0.9,
-              // height: MediaQuery.of(context).size.height,
-              child: child,
-            ),
-          );
-        }
-      }),
-    );
+    return LayoutBuilder(builder: (context, constraint) {
+      if (constraint.maxWidth < 1000) {
+        return Container(
+          color: Colors.white,
+          width: MediaQuery.of(context).size.width,
+          // height: MediaQuery.of(context).size.height,
+          child: child,
+        );
+      } else if (constraint.maxWidth >= 1000 && constraint.maxWidth < 1200) {
+        return Container(
+          margin: EdgeInsets.symmetric(
+              horizontal: MediaQuery.of(context).size.width * 0.1,
+              vertical: 10),
+          color: Colors.white,
+          width: MediaQuery.of(context).size.width * 0.9,
+          // height: MediaQuery.of(context).size.height,
+          child: child,
+        );
+      } else {
+        return Container(
+          margin: EdgeInsets.symmetric(
+              horizontal: MediaQuery.of(context).size.width * 0.15,
+              vertical: 10),
+          color: Colors.white,
+          width: MediaQuery.of(context).size.width * 0.85,
+          // height: MediaQuery.of(context).size.height,
+          child: child,
+        );
+      }
+    });
   }
 }
