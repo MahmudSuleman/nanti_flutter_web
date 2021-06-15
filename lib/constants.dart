d00b300d8051095af5fffabee0859d61ba2871b6
Mahmud Chamtooni <mudisheikh@outlook.com>
2021-06-15 16:27:12 +0000

    change base url

--- lib/constants.dart
+++ lib/constants.dart
@@ -3,4 +3,4 @@ import 'package:flutter/material.dart';
 final kPageHeaderTextStyle =
     TextStyle(fontSize: 30, fontWeight: FontWeight.bold);
 
-final kBaseUrl = 'http://78b47b242cd9.ngrok.io/nanti_flutter_web_api/endpoint/';
+final kBaseUrl = 'http://10.0.2.2/nanti_flutter_web_api/endpoint/';
