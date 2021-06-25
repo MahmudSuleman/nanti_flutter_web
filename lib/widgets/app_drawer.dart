b50a6ca2bdb5ad131ea7aa46b9469358b8f6b214
Mahmud Chamtooni <mudisheikh@outlook.com>
2021-06-25 13:20:21 +0000

    separate user appBar from admin appBar

--- lib/widgets/app_drawer.dart
+++ lib/widgets/app_drawer.dart
@@ -4,87 +4,142 @@ import 'package:nanti_flutter_web/screens/device_list.dart';
 import 'package:nanti_flutter_web/screens/dispatch_list.dart';
 import 'package:nanti_flutter_web/screens/login.dart';
 import 'package:nanti_flutter_web/screens/maintenance_list.dart';
+import 'package:nanti_flutter_web/screens/user/user_device_list.dart';
 import 'package:nanti_flutter_web/screens/user_list.dart';
 import 'package:nanti_flutter_web/services/auth_service.dart';
 import 'package:nanti_flutter_web/user_prefs.dart';
 
-class AppDrawer extends StatelessWidget {
+class AppDrawer extends StatefulWidget {
+  @override
+  _AppDrawerState createState() => _AppDrawerState();
+}
+
+class _AppDrawerState extends State<AppDrawer> {
+  bool isAdmin = false;
+  @override
+  void didChangeDependencies() {
+    super.didChangeDependencies();
+    AuthService.isAdmin().then((value) {
+      setState(() {
+        isAdmin = value;
+      });
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
+    AuthService.isAdmin().then((value) {
+      setState(() {});
+      isAdmin = value;
+    });
     return Drawer(
       child: ListView(
         padding: EdgeInsets.zero,
-        children: <Widget>[
-          _createHeader(),
-          divider(),
-          _createDrawerItem(
-            context,
-            icon: Icons.home,
-            text: 'Dashboard',
-            onTap: () {
-              Navigator.pushReplacementNamed(context, '/');
-            },
-          ),
-          divider(),
-          _createDrawerItem(
-            context,
-            icon: Icons.phone_android,
-            text: 'Devices',
-            onTap: () {
-              Navigator.pushNamed(context, DeviceList.routeName);
-            },
-          ),
-          divider(),
-          _createDrawerItem(
-            context,
-            icon: Icons.house,
-            text: 'Companies',
-            onTap: () {
-              Navigator.pushNamed(context, CompanyList.routeName);
-            },
-          ),
-          divider(),
-          _createDrawerItem(
-            context,
-            icon: Icons.send,
-            text: 'Dispatch',
-            onTap: () {
-              Navigator.pushNamed(context, DispatchList.routeName);
-            },
-          ),
-          divider(),
-          _createDrawerItem(
-            context,
-            icon: Icons.settings,
-            text: 'Maintenance',
-            onTap: () {
-              Navigator.pushNamed(context, MaintenanceList.routeName);
-            },
-          ),
-          divider(),
-          _createDrawerItem(
-            context,
-            icon: Icons.person_add,
-            text: 'Users',
-            onTap: () {
-              Navigator.pushNamed(context, UserList.routeName);
-            },
-          ),
-          divider(),
-          _createDrawerItem(
-            context,
-            icon: Icons.logout,
-            text: 'Logout',
-            onTap: () {
-              UserPrefs.clearUser().then((_) {
-                Navigator.pushReplacementNamed(context, Login.routeName);
-              });
-            },
-          ),
-        ],
+        children: isAdmin ? adminAppDrawer(context) : userAppDrawer(context),
       ),
     );
   }
+
+  List<Widget> userAppDrawer(context) => [
+        _createHeader(),
+        _createDrawerItem(
+          context,
+          icon: Icons.home,
+          text: 'Dashboard',
+          onTap: () {
+            Navigator.pushReplacementNamed(context, '/');
+          },
+        ),
+        divider(),
+        _createDrawerItem(
+          context,
+          icon: Icons.list,
+          text: 'Device List',
+          onTap: () {
+            Navigator.pushNamed(context, UserDeviceList.routeName);
+          },
+        ),
+        divider(),
+        _createDrawerItem(
+          context,
+          icon: Icons.logout,
+          text: 'Logout',
+          onTap: () {
+            UserPrefs.clearUser().then((_) {
+              Navigator.pushReplacementNamed(context, Login.routeName);
+            });
+          },
+        ),
+      ];
+
+  List<Widget> adminAppDrawer(context) => [
+        _createHeader(),
+        divider(),
+        _createDrawerItem(
+          context,
+          icon: Icons.home,
+          text: 'Dashboard',
+          onTap: () {
+            Navigator.pushReplacementNamed(context, '/');
+          },
+        ),
+        divider(),
+        _createDrawerItem(
+          context,
+          icon: Icons.phone_android,
+          text: 'Devices',
+          onTap: () {
+            Navigator.pushNamed(context, DeviceList.routeName);
+          },
+        ),
+        divider(),
+        _createDrawerItem(
+          context,
+          icon: Icons.house,
+          text: 'Companies',
+          onTap: () {
+            Navigator.pushNamed(context, CompanyList.routeName);
+          },
+        ),
+        divider(),
+        _createDrawerItem(
+          context,
+          icon: Icons.send,
+          text: 'Dispatch',
+          onTap: () {
+            Navigator.pushNamed(context, DispatchList.routeName);
+          },
+        ),
+        divider(),
+        _createDrawerItem(
+          context,
+          icon: Icons.settings,
+          text: 'Maintenance',
+          onTap: () {
+            Navigator.pushNamed(context, MaintenanceList.routeName);
+          },
+        ),
+        divider(),
+        _createDrawerItem(
+          context,
+          icon: Icons.person_add,
+          text: 'Users',
+          onTap: () {
+            Navigator.pushNamed(context, UserList.routeName);
+          },
+        ),
+        divider(),
+        _createDrawerItem(
+          context,
+          icon: Icons.logout,
+          text: 'Logout',
+          onTap: () {
+            UserPrefs.clearUser().then((_) {
+              Navigator.pushReplacementNamed(context, Login.routeName);
+            });
+          },
+        ),
+      ];
 
   Divider divider() => Divider(
         color: Colors.green,
