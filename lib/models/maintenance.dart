25d07bed0577a163426bd1f38c74c4df4ce0e187
Mahmud Chamtooni <mudisheikh@outlook.com>
2021-06-25 15:11:22 +0000

    add nullable company id to the class properties

--- lib/models/maintenance.dart
+++ lib/models/maintenance.dart
@@ -4,18 +4,22 @@ class Maintenance {
   String companyName;
   String deviceName;
   String problemDescription;
+  String? companyId;
 
-  Maintenance(
-      {required this.id,
-      required this.deviceName,
-      required this.companyName,
-      required this.dateSent,
-      required this.problemDescription});
+  Maintenance({
+    required this.id,
+    required this.deviceName,
+    required this.companyName,
+    required this.dateSent,
+    required this.problemDescription,
+    this.companyId,
+  });
 
   Maintenance.fromJson(Map<String, dynamic> json)
       : id = json['id'],
         deviceName = json['deviceName'],
         companyName = json['companyName'],
         dateSent = json['dateSent'],
+        companyId = json['companyId'],
         problemDescription = json['problemDescription'];
 }
