44adb0ec9ad912d630caadb335ee0556dc389007
Mahmud Chamtooni <mudisheikh@outlook.com>
2021-09-28 08:36:07 +0000

    change company list table size

--- lib/screens/company/company_list.dart
+++ lib/screens/company/company_list.dart
@@ -83,91 +83,95 @@ class _CompanyListState extends State<CompanyList> {
           )
         : SingleChildScrollView(
             scrollDirection: Axis.horizontal,
-            child: DataTable(
-                columns: _tableHeader
-                    .map((e) => DataColumn(label: Text(e)))
-                    .toList(),
-                rows: [
-                  for (Company item in data)
-                    DataRow(cells: [
-                      DataCell(Text(item.name)),
-                      DataCell(Text(item.type)),
-                      DataCell(Text(item.contact)),
-                      DataCell(Row(
-                        children: [
-                          ElevatedButton(
-                            style: kElevatedButtonStyle(),
-                            onPressed: () {
-                              showDialog(
-                                  context: context,
-                                  builder: (context) {
-                                    return AlertDialog(
-                                      content: EditCompany(
-                                        new Company(
-                                          id: item.id,
-                                          name: item.name,
-                                          type: item.type,
-                                          contact: item.contact,
+            child: Container(
+              width: 1000,
+              child: DataTable(
+                  columns: _tableHeader
+                      .map((e) => DataColumn(label: Text(e)))
+                      .toList(),
+                  rows: [
+                    for (Company item in data)
+                      DataRow(cells: [
+                        DataCell(Text(item.name)),
+                        DataCell(Text(item.type)),
+                        DataCell(Text(item.contact)),
+                        DataCell(Row(
+                          children: [
+                            ElevatedButton(
+                              style: kElevatedButtonStyle(),
+                              onPressed: () {
+                                showDialog(
+                                    context: context,
+                                    builder: (context) {
+                                      return AlertDialog(
+                                        content: EditCompany(
+                                          new Company(
+                                            id: item.id,
+                                            name: item.name,
+                                            type: item.type,
+                                            contact: item.contact,
+                                          ),
                                         ),
-                                      ),
-                                    );
-                                  }).then((value) => setState(() {}));
-                            },
-                            child: Icon(Icons.edit),
-                          ),
-                          SizedBox(
-                            width: 10,
-                          ),
-                          ElevatedButton(
-                            style: kElevatedButtonStyle(color: Colors.red),
-                            onPressed: () {
-                              showDialog(
-                                context: context,
-                                builder: (context) => AlertDialog(
-                                  content: Text(
-                                      'Are you sure you want to delete this item?'),
-                                  actions: [
-                                    MaterialButton(
-                                      onPressed: () async {
-                                        var res = await CompanyService.destroy(
-                                            item.id);
+                                      );
+                                    }).then((value) => setState(() {}));
+                              },
+                              child: Icon(Icons.edit),
+                            ),
+                            SizedBox(
+                              width: 10,
+                            ),
+                            ElevatedButton(
+                              style: kElevatedButtonStyle(color: Colors.red),
+                              onPressed: () {
+                                showDialog(
+                                  context: context,
+                                  builder: (context) => AlertDialog(
+                                    content: Text(
+                                        'Are you sure you want to delete this item?'),
+                                    actions: [
+                                      MaterialButton(
+                                        onPressed: () async {
+                                          var res =
+                                              await CompanyService.destroy(
+                                                  item.id);
 
-                                        if (res) {
+                                          if (res) {
+                                            Navigator.pop(context);
+                                            ScaffoldMessenger.of(context)
+                                                .showSnackBar(SnackBar(
+                                                    content:
+                                                        Text('Data deleted!')));
+                                          } else {
+                                            Navigator.pop(context);
+                                            ScaffoldMessenger.of(context)
+                                                .showSnackBar(SnackBar(
+                                                    content: Text(
+                                                        'Failed to delete device data!')));
+                                          }
+                                        },
+                                        // color: Colors.red,
+                                        child: Text('Yes'),
+                                      ),
+                                      MaterialButton(
+                                        onPressed: () {
                                           Navigator.pop(context);
-                                          ScaffoldMessenger.of(context)
-                                              .showSnackBar(SnackBar(
-                                                  content:
-                                                      Text('Data deleted!')));
-                                        } else {
-                                          Navigator.pop(context);
-                                          ScaffoldMessenger.of(context)
-                                              .showSnackBar(SnackBar(
-                                                  content: Text(
-                                                      'Failed to delete device data!')));
-                                        }
-                                      },
-                                      // color: Colors.red,
-                                      child: Text('Yes'),
-                                    ),
-                                    MaterialButton(
-                                      onPressed: () {
-                                        Navigator.pop(context);
-                                      },
-                                      // color: Colors.red,
-                                      child: Text('No'),
-                                    )
-                                  ],
-                                ),
-                              ).then((value) {
-                                setState(() {});
-                              });
-                            },
-                            child: Icon(Icons.delete),
-                          ),
-                        ],
-                      )),
-                    ])
-                ]),
+                                        },
+                                        // color: Colors.red,
+                                        child: Text('No'),
+                                      )
+                                    ],
+                                  ),
+                                ).then((value) {
+                                  setState(() {});
+                                });
+                              },
+                              child: Icon(Icons.delete),
+                            ),
+                          ],
+                        )),
+                      ])
+                  ]),
+            ),
           );
   }
 }
