41ebf656fadcc55ad7e067e9467d2831292b19f6
Mahmud Chamtooni <mudisheikh@outlook.com>
2021-11-08 16:58:22 +0000

    test fix

--- lib/services/auth_service.dart
+++ lib/services/auth_service.dart
@@ -41,14 +41,12 @@ class AuthService {
     return user != null;
   }
 
-  static Future<bool> isAdmin() async {
-    var res = await isLoggedIn();
-    if (res) {
-      var user = await UserPrefs.getUserPrefs();
-      if (user?.companyId == '0') {
-        return true;
-      }
-    }
-    return false;
-  }
+  // static Future<bool> isAdmin() async {
+  //   var res = await isLoggedIn();
+  //   if (res) {
+  //     var user = await UserPrefs.getCompany();
+  //     return user == 0;
+  //   }
+  //   return false;
+  // }
 }
