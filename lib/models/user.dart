d0c2abda5bfb61f1648b8cfb9331f24ff26c00ad
Mahmud Chamtooni <mudisheikh@outlook.com>
2021-11-13 18:11:36 +0000

    modify user model

--- lib/models/user.dart
+++ lib/models/user.dart
@@ -1,18 +1,27 @@
+import 'package:nanti_flutter_web/models/client.dart';
+
 class User {
-  String id;
-
-  String username;
-
-  String companyId;
+  int id;
+  String name;
+  String email;
+  int clientId;
+  Client? client;
 
   User({
     required this.id,
-    required this.username,
-    required this.companyId,
+    required this.name,
+    required this.email,
+    required this.clientId,
+    this.client,
   });
 
   User.fromJson(Map<String, dynamic> json)
       : id = json['id'],
-        username = json['username'],
-        companyId = json['companyId'];
+        name = json['name'],
+        email = json['email'],
+        clientId = json['client_id'].runtimeType == int
+            ? json['client_id']
+            : int.parse(json['client_id']),
+        client =
+            json['client'] == null ? null : Client.fromJson(json['client']);
 }
