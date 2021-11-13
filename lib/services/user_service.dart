fdd973ca12f0a8a32f9537226d26e8596265a598
Mahmud Chamtooni <mudisheikh@outlook.com>
2021-11-13 18:11:23 +0000

    modify user route name

--- lib/services/user_service.dart
+++ lib/services/user_service.dart
@@ -5,27 +5,27 @@ import 'package:nanti_flutter_web/constants.dart';
 import 'package:nanti_flutter_web/models/user.dart';
 
 class UserService {
-  static String baseUrl = kBaseUrl + 'users/';
+  static String baseUrl = kBaseUrl + '/user';
 
-  static Future<List<User>> allUsers() async {
-    List<User> temp = [];
+  static Future<List<User>?> index() async {
     try {
-      var url = Uri.parse(baseUrl + 'index.php');
-      var response = await http.get(url, headers: kHeaders);
+      List<User> temp = [];
+      var url = Uri.parse(baseUrl);
+      var response = await http.get(url);
       if (response.statusCode == 200) {
         var data = jsonDecode(response.body) as List<dynamic>;
-
-        if (data != []) {
-          data.forEach((element) {
-            temp.add(User.fromJson(element));
-          });
-        }
+        data.forEach((element) {
+          temp.add(User.fromJson(element));
+        });
+        return temp;
+      } else {
+        return null;
       }
     } catch (error) {
       print(StackTrace.current);
     }
 
-    return temp;
+    return null;
   }
 
   static Future<bool> store(username, company, password) async {
